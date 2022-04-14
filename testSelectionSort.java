d521e21b682bb472cebe3142fadb354c7e50b782
NickShute <nshute@umich.edu>
2022-04-13 20:41:05 -0400

    Fixed branch1

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -5,6 +5,8 @@ public class testSelectionSort {
 public void test() {
 testPositive();
 testNegative();
+//testMixed();
+//testDuplicates();
 }
     public testSelectionSort() {
     }
@@ -69,4 +71,66 @@ testNegative();
  	    	}
  	    	System.out.println("testNegative passed");
     }
+    /*public void testMixed(){
+
+    int[] arr = new int[5];
+    arr[0] = 0;
+    arr[1] = -3;
+    arr[2] = 8;
+    arr[3] = 2;
+    arr[4] = -9;
+
+    int[] Sortedarr = new int[5];
+    Sortedarr[0] = -9;
+    Sortedarr[1] = -3;
+    Sortedarr[2] = 0;
+    Sortedarr[3] = 2;
+    Sortedarr[4] = 8;
+
+    SelectionSort selectionSort = new SelectionSort();
+
+    int[] arrSorted = new int[5];
+    arrSorted = selectionSort.basicSelectionSort(arr);
+
+    try {
+     assertEquals(Sortedarr[4], arrSorted[4]);
+     }
+     catch (AssertionError e) {
+        System.out.println("testMixed failed");
+        throw e;
+    }
+    System.out.println("testMixed passed");
+}
+
+  public void testDuplicates(){
+
+      int[] arr = new int[5];
+      arr[0] = -3;
+      arr[1] = 8;
+      arr[2] = 0;
+      arr[3] = 8;
+      arr[4] = -2;
+
+      int[] Sortedarr = new int[5];
+      Sortedarr[0] = -3;
+      Sortedarr[1] = -2;
+      Sortedarr[2] = 0;
+      Sortedarr[3] = 8;
+      Sortedarr[4] = 8;
+
+      SelectionSort selectionSort = new SelectionSort();
+
+      int[] arrSorted = new int[5];
+      arrSorted = selectionSort.basicSelectionSort(arr);
+
+      try {
+       assertEquals(Sortedarr[3], arrSorted[3]);
+       }
+       catch (AssertionError e) {
+          System.out.println("testDuplicates failed");
+          throw e;
+      }
+      System.out.println("testDuplicates passed");
+  }*/
+
 }
